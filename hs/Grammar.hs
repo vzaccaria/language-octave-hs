363c770b6c113b86006abacf7152bd1a3500e6c3
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-05-11 17:55:38 +0200

    Renamed files

---
+++ hs/Grammar.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module OctaveGrammar where
+
+import           Control.Applicative                 hiding (many, (<|>))
+import           OctaveLexer
+import           PrettyPrint
+import           Text.ParserCombinators.Parsec
+import           Text.ParserCombinators.Parsec.Error
+
+
+_statement :: Parser String
+_statement = _ws >> (_id <|> _stringLiteral)
+
+_statements :: Parser [String]
+_statements = endBy _statement _eos
+
+
+errorAsMessages :: ParseError -> [String]
+errorAsMessages theError = fmap messageString (errorMessages theError)
+
+theParser:: Parser String
+theParser = _ws >> (fmap asSet _statements) <* eof
+
+
+
+-- Utility function (to string)
+parseIt :: String -> Either ParseError String
+parseIt = parse theParser "(source)"
+
+justParse :: String -> IO String
+justParse s = return $
+          case parsed of
+            Right value -> "It works! just lexed: " ++ value
+            Left theError -> let
+              errorString = asSet $ errorAsMessages theError
+              in "Error!! " ++ errorString
+          where parsed = parseIt s
---
+++ hs/Lexer.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module OctaveLexer  where
+
+import           Data.Functor.Identity
+import           Text.ParserCombinators.Parsec
+import           Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token    as Token
+
+-- We start from a classic definition for tokens in our language
+octaveDef :: GenLanguageDef String u Identity
+octaveDef = emptyDef {
+  Token.commentLine     = "%",
+
+  Token.identStart      = letter,
+
+  Token.identLetter     = alphaNum,
+
+  Token.reservedNames   = [ "if" , "then" , "else" , "while" , "do",
+                            "skip" , "end" , "true" , "false" ],
+
+  Token.reservedOpNames = [   "+", "-", "*", "/", "=",
+                              "~=", "==", "^", "./", ".*",
+                              "<", ">", "<=", ">=", ":", "'",
+                              "&", "&&", "|", "||" ]
+}
+
+lexer :: Token.GenTokenParser String u Data.Functor.Identity.Identity
+lexer = Token.makeTokenParser octaveDef
+
+-- Derive your own microParsers to be combined here:
+
+_lexeme:: Parser a -> Parser a
+_lexeme = Token.lexeme lexer
+
+
+_id:: Parser String
+_id = _lexeme $ do {
+        c <- letter <|> char '_';
+        cs <- many (alphaNum <|> char '_');
+        return (c:cs)
+        }
+
+_symbol :: String -> Parser String
+_symbol = Token.symbol lexer
+
+_int :: Parser Integer
+_int = _lexeme (Token.integer lexer)
+
+_double :: Parser Double
+_double = _lexeme (Token.float lexer)
+
+_stringLiteral :: Parser String
+_stringLiteral = _lexeme s
+                 where
+                   legitChar = noneOf [ '\'', '\n' ]
+                   s = between (char '\'') (char '\'') (many legitChar)
+
+_reserved:: String -> Parser ()
+_reserved = Token.reservedOp lexer
+
+_ws :: Parser ()
+_ws = Token.whiteSpace lexer
+
+_eol :: Parser Char
+_eol = newline
+
+_parens:: Parser a -> Parser a
+_parens = Token.parens lexer
+
+_brackets:: Parser a -> Parser a
+_brackets = Token.brackets lexer
+
+_semi :: Parser Char
+_semi = char ';'
+
+_eos :: Parser Char
+_eos = (_eol <|> _semi)
