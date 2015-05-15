886dca3d90d220d6c1ac9ed670fa820b5f83471a
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-05-15 12:42:49 +0200

    use a single makefile

--- configure.js
+++ configure.js
@@ -4,7 +4,8 @@ var {
 
 var path = require('path')
 
-generateProject(_ => {
+var initializeHooks = _ => {
+  "use strict"
 
   _.babel = (dir, ...deps) => {
     var command = (_) => `./node_modules/.bin/babel ${_.source} -o ${_.product}`
@@ -12,24 +13,91 @@ generateProject(_ => {
     _.compileFiles(...([command, product, dir].concat(deps)))
   }
 
-
-
   _.verb = (verbfile, deps) => {
     var command = () => `./node_modules/.bin/verb`
     var product = () => `./readme.md`
     _.compileFiles(...([command, product, verbfile].concat(deps)))
   }
 
-  _.collectSeq("all", _ => {
-    _.collect("build", _ => {
-      _.babel("src/*.js")
-      _.cmd("cd hs && make")
+  _.ghcjs = (dir, ...deps) => {
+    var command = (_) =>
+      `ghcjs -DGHCJS_BROWSER -ihs ${_.source} -o HS.x
+\techo "(function(global) {" > ${_.product}
+\tcat HS.x/{rts,lib,out}.js ./hs/Exports.js >> ${_.product}
+\techo "})(window);" >> ${_.product}
+\trm -rf HS.x
+`
+    var product = (_) => `./${path.basename(_.source, '.hs')}.js`
+    _.compileFiles(...([command, product, dir].concat(deps).concat('./hs/Exports.js')))
+  }
+
+  _.browserify = (dir, ...deps) => {
+    var command = (_) => `./node_modules/.bin/browserify -t babelify -t node-lessify  ${_.source} -o ${_.product}`
+    var product = (_) => `${_.source.replace(/\..*/, '.bfd.js')}`
+    _.compileFiles(...([command, product, dir].concat(deps)))
+  }
+
+  _.jadeify = (dir, ...deps) => {
+    var command = (_) => `./node_modules/.bin/jade ${_.source}`
+    var product = (_) => `${_.source.replace(/\..*/, '.html')}`
+    _.compileFiles(...([command, product, dir].concat(deps)))
+  }
+}
+
+var produceWeb = _ => {
+  "use strict"
+  _.collect("build-web", _ => {
+    _.toFile("_site/index.html", _ => {
+      _.jadeify("web/src/index.jade")
     })
-    _.cmd("cp ./hs/Octave.js ./lib")
-    _.cmd("cp ./lib/index.js ./index.js")
-    _.cmd("cd web && make")
-    _.cmd("./node_modules/.bin/markdox ./index.js -o docs/api.md")
+
+    _.toFile("_site/client.js", _ => {
+      _.concat(_ => {
+        _.browserify("web/src/index.js", "web/src/*.less", "web/src/*.css")
+      })
+      _.ghcjs("./hs/Octave.hs")
+    })
+  })
+}
+
+var produceHaskell = _ => {
+  "use strict"
+  _.collect("build-haskell", _ => {
+    _.toFile("./lib/Octave.js", _ => {
+      _.ghcjs("./hs/Octave.hs")
+    })
+  })
+}
+
+var produceDocs = _ => {
+  "use strict"
+  _.collectSeq("docs", _ => {
+    // _.cmd("./node_modules/.bin/markdox ./index.js -o docs/api.md")
     _.verb("./verbfile.js", "docs/*.md")
+  })
+}
+
+var cleanTargets = _ => {
+  _.collect("clean-dev", _ => {
+    _.cmd("make clean")
+    _.cmd("rm -rf hs/*.js_o")
+    _.cmd("rm -rf hs/*.js_hi")
+  })
+}
+
+
+
+generateProject(_ => {
+  "use strict"
+
+  initializeHooks(_)
+
+  _.collect("all", _ => {
+    _.collectSeq("build", _ => {
+      produceHaskell(_)
+      produceWeb(_)
+    })
+    produceDocs(_)
   })
 
   _.collect("test", _ => {
@@ -46,6 +114,18 @@ generateProject(_ => {
       _.cmd(`make all`)
       _.cmd(`./node_modules/.bin/xyz -i ${it}`)
     })
+
   })
 
+  cleanTargets(_)
+
+  _.collect("start", _ => {
+    _.startWatch("_site/**/*")
+    _.startServe("_site")
+  })
+
+  _.collect("stop", _ => {
+    _.stopWatch()
+    _.stopServe()
+  })
 })
